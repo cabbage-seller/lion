da3787df9436a99d597ffa23b02937f86a8f295c
Jung Hoon Kim <junghoonkim@Jungs-MacBook-Air.local>
2026-05-19 22:11:20 +0900

    #2 Navigation Guard

--- src/router/index.js
+++ src/router/index.js
@@ -9,23 +9,46 @@ const router = createRouter({
     routes: [
         {
             path: '/',
-            component: LoginPage
+            component: LoginPage,
+            beforeEnter: (to, from, next) => {
+                const token = localStorage.getItem('access_token')
+                if (token) {
+                    next('/main')
+                } else {
+                    next()
+                }
+
+            }
         },
         {
             path: '/demo',
             component: DemoPage
         },
         {
-            path: '/login',
-            component: LoginPage
-        },
-        {
             path: '/signup',
-            component: SignUpPage
+            component: SignUpPage,
+            beforeEnter: (to, from, next) => {
+                const token = localStorage.getItem('access_token')
+                if (token) {
+                    next('/main')
+                } else {
+                    next()
+                }
+
+            }
         },
         {
             path: '/main',
-            component: MainPage
+            component: MainPage,
+            beforeEnter: (to, from, next) => {
+                const token = localStorage.getItem('access_token')
+                if (token) {
+                    next()
+                } else {
+                    next('/')
+                }
+
+            }
         }
     ]
 })
